c5eb1a8483ff49e3b915b05c25388672867aaa70
aishatahir3023 <160772524+aishatahir3023@users.noreply.github.com>
2024-12-06 03:07:02 +0500

    3rd commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,14 @@
 import { Container } from "postcss";
 import type { Config } from "tailwindcss";
+import { NextConfig } from 'next';
+
+const config: NextConfig = {
+  reactStrictMode: true,
+};
+
+
+
+
 
 export default {
   content: [
